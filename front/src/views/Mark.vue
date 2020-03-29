acec33cc879d6b7eb07703ab9627d0cb69e16a30
zyxnine9 <jianghurenzhe@qq.com>
2020-03-29 20:00:16 +0800

    解决

---
+++ front/src/views/Mark.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    this is mark
+    {{imgs}}
+    <el-carousel 
+    :autoplay="false"
+    :loop="false"  
+    indicator-position="none" 
+    ref="ques">
+      <el-carousel-item v-for="img in imgs" :key="img">
+        <img :src=img alt="">
+      </el-carousel-item>
+    </el-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  data() {
+    return {
+      images: []
+    };
+  },
+  computed:{
+    imgs(){
+      console.log(this.images)
+      return this.images.map(e=>"http://localhost:5000/image/"+e)
+    }
+  },
+  created() {
+    this.images = this.$route.params.images;
+  }
+};
+</script>
+
+<style lang="less" scoped>
+</style>
